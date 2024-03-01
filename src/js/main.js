2df1e274588ec6d38831674cf9acd401bda94a34
Tatiana Krupnikova <ilm63.rus@gmail.com>
2024-03-01 12:14:13 +0300

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+//Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+//переменные для элементов DOM
+
+
+
+//Код Веры
+
+
+
+// Код Нади
+
+
+
+// Код Насти
+
+
+
+// Код Тани
