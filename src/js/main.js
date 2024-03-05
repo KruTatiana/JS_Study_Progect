5cfb41b123d1bb12d397f1dc826dbb7a1ee77636
Nastya <nshpv@icloud.comgit config --global user.email n.shpv@icloud.com'git config --global user.email n.shpv@icloud.com>
2024-03-05 18:29:14 +0300

    menu added

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,77 @@
 //Import our custom CSS
-import '../scss/styles.scss'
-
+import "../scss/styles.scss";
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
 
 //Import moment library
-var moment = require('moment');
+var moment = require("moment");
 moment().format();
 
 //переменные для элементов DOM
-const addTaskBtn = document.querySelector('.add_task');
-const taskName = document.getElementById('task_name');
-const taskDescription = document.getElementById('task_description');
-
+const addTaskBtn = document.querySelector(".add_task");
+const taskName = document.getElementById("task_name");
+const taskDescription = document.getElementById("task_description");
 
 //Код Веры
 
-
-
 // Код Нади
 function makeQuote() {
-	fetch('https://stoic.tekloon.net/stoic-quote')
-	.then((res) => {
-		return res.json();
-})
-	.then((res) => {
-		let quote = document.getElementById("quote");
-		let author = document.getElementById('author');
-		quote.textContent = res.quote; 
-		author.textContent = res.author;
-})
-.catch((err) => {
-    console.log('Произошла ошибка');
-  });
+    fetch("https://stoic.tekloon.net/stoic-quote")
+        .then((res) => {
+            return res.json();
+        })
+        .then((res) => {
+            let quote = document.getElementById("quote");
+            let author = document.getElementById("author");
+            quote.textContent = res.quote;
+            author.textContent = res.author;
+        })
+        .catch((err) => {
+            console.log("Произошла ошибка");
+        });
 }
 
-window.addEventListener('load', () => {
-	makeQuote();
-  });
-
+window.addEventListener("load", () => {
+    makeQuote();
+});
 
 // Код Насти
 
+const tasksLinks = document.getElementsByClassName("tasksLink");
 
+const menuListContainer = document.getElementById("menuListContainer");
+
+// добавить заголовок на стринцу !!
+const title = document.getElementById("title");
+
+menuListContainer.addEventListener("click", function (evt) {
+    const eventTarget = evt.target;
+    // получание на стрианице группы задач
+    // получить коллекцию из всех задач tasks !!
+    // for (let i = 0; i < tasks.length; i += 1) {
+    // const task=tasks[i]
+    //     if (task.classList.contains(eventTarget.id)) {
+    //         task.classList.remove("disabled");
+    //     } else {
+    //         task.classList.add("disabled");
+    //     }
+    // }
+    // отображаем в меню выбранный раздел
+    for (let i = 0; i < tasksLinks.length; i += 1) {
+        if (tasksLinks[i].id === eventTarget.id) {
+            tasksLinks[i].classList.add("active-link");
+        } else {
+            tasksLinks[i].classList.remove("active-link");
+        }
+    }
+    // отображаем в заголовке выбранный раздел
+    // if (eventTarget.id === "anyTask" || eventTarget.classList.contains("categoryLink")) {
+    //     title.textContent = eventTarget.textContent;
+    // } else if (eventTarget.classList.contains("simpleLink")) {
+    //     title.textContent = eventTarget.textContent + " задачи";
+    // } else if (eventTarget.classList.contains("priorityLink")) {
+    //     title.textContent = eventTarget.textContent + " приоритет";
+    // }
+});
 
 // Код Тани
