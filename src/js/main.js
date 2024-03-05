6176be2fb7df5aed8456928534abcf02fc107831
Kururpira <kururpira@inbox.ru>
2024-03-05 11:06:05 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -8,13 +8,21 @@ var moment = require("moment");
 moment().format();
 
 //переменные для элементов DOM
-const saveTaskBtn = document.querySelector(".save_task__btn");
-const tasksList = document.querySelector(".tasks_list");
-const taskName = document.getElementById("task_name");
-const taskDescription = document.getElementById("task_description");
-const deadlineDate = document.querySelector(".end_date");
-const deadlineTime = document.querySelector(".end_time");
-const todayDate = document.querySelector(".today_date");
+const saveTaskBtn = document.querySelector('.save_task__btn');
+const tasksList = document.querySelector('.tasks_list');
+const taskName = document.getElementById('task_name');
+const taskDescription = document.getElementById('task_description');
+const deadlineDate = document.querySelector('.end_date');
+const deadlineTime = document.querySelector('.end_time');
+const todayDate = document.querySelector('.today_date');
+let ingridients = document.getElementById('ingridients');
+let button = document.getElementById('button');
+let sugar = document.getElementById('sugar');
+let iron = document.getElementById('iron');
+let vitaminC = document.getElementById('vitaminC');
+let calories = document.getElementById('calories');
+let calcium = document.getElementById('calcium');
+let analysisForm = document.getElementById('AnalysisForm');
 // const priorityHigh = document.getElementById('priority_btn-high');
 // const priorityMedium = document.getElementById('priority_btn-medium');
 // const priorityLow = document.getElementById('priority_btn-low');
@@ -26,6 +34,28 @@ const todayDate = document.querySelector(".today_date");
 // const partOther = document.getElementById('part_btn-other');
 
 //Код Веры
+function onBtnClick(){
+	let ingridientsValue = ingridients.value;
+  //const API = 'https://api.edamam.com/api/nutrition-data?app_id=d7be0f59&app_key=7670b7efd74aa8278e4343bfd8644a49&nutrition-type=cooking&ingr=1%20onion'
+  let API = `https://api.edamam.com/api/nutrition-data?app_id=d7be0f59&app_key=7670b7efd74aa8278e4343bfd8644a49&nutrition-type=cooking&ingr=1%20${ingridientsValue}`;
+  
+  let caloriesResult = fetch(API).then((res) => res.json()).then((data) => calories.textContent = Math.ceil(data.totalNutrients.ENERC_KCAL.quantity));
+  let sugarResult = fetch(API).then((res) => res.json()).then((data) => sugar.textContent = Math.ceil(data.totalNutrients.SUGAR.quantity));
+  let ironResult = fetch(API).then((res) => res.json()).then((data) => iron.textContent = Math.ceil(data.totalNutrients.FE.quantity));
+  let vitaminCResult = fetch(API).then((res) => res.json()).then((data) => vitaminC.textContent = Math.ceil(data.totalNutrients.VITC.quantity));
+  let calciumCResult = fetch(API).then((res) => res.json()).then((data) => calcium.textContent = Math.ceil(data.totalNutrients.CA.quantity));
+	}
+  
+  function onBtnClickCleaner(){
+   
+calories.textContent = 0;
+  sugar.textContent = 0;
+  iron.textContent = 0;
+  vitaminC.textContent = 0;
+  calcium.textContent = 0;
+  ingridients.value = '';
+  }
+
 
 // Код Нади
 function makeQuote() {
