ba8da9da67b08c86aa371d9f6198550aa43e30f4
Tatiana Krupnikova <ilm63.rus@gmail.com>
2024-03-06 14:38:45 +0300

    Json obj in Local Storage

--- src/js/main.js
+++ src/js/main.js
@@ -195,15 +195,17 @@ let deadline;
 //класс для сборки карточек задач
 
 class taskCard {
-    constructor(name, description, deadline, color, lifepart) {
+    constructor(name, description, deadline, color, lifepart, deadlineDate, deadlineTime, id, checkbox) {
         this.name = name;
         this.description = description;
         this.deadline = deadline;
         this.color = color;
         this.lifePart = lifepart;
+        this.deadlineDate = deadlineDate;
+        this.deadlineTime = deadlineTime;
+        this.id = id;
+        this.checkbox = checkbox;
     }
-
-    makeObj() {}
 
     createTask() {
         this.element = document.createElement("div");
@@ -218,17 +220,21 @@ class taskCard {
         this.element.appendChild(this.priorityLifeEl);
         this.element.appendChild(this.checkEl);
         this.element.appendChild(this.contentBoxEl);
+        this.contentBoxEl.appendChild(this.partLifeEl);
         this.contentBoxEl.appendChild(this.nameEl);
         this.contentBoxEl.appendChild(this.descriptionEl);
         this.element.appendChild(this.deadlineEl);
         this.element.setAttribute("class", "new_task_element");
         this.checkEl.setAttribute("type", "checkbox");
         this.checkEl.setAttribute("class", "task_checkbox");
+        this.checkEl.setAttribute("id", this.id);
+        this.checkEl.setAttribute("onclick", "addCheck(this)");
+        this.partLifeEl.setAttribute("class", "part_life_element");
         this.nameEl.setAttribute("class", "task_name_text");
         this.descriptionEl.setAttribute("class", "description_text");
         this.priorityLifeEl.setAttribute("class", this.color);
         this.contentBoxEl.setAttribute("class", "content_task_box");
-        this.partLifeEl.textContent = this.lifePart;
+        this.partLifeEl.innerText = this.lifePart;
         this.nameEl.innerText = this.name;
         this.descriptionEl.innerText = this.description;
         this.deadlineEl.innerText = this.deadline;
