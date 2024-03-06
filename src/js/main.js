6ce2616ef4342a964dda24310937a00e17020a7a
Tatiana Krupnikova <ilm63.rus@gmail.com>
2024-03-06 15:00:34 +0300

    Button Clean Local Storage

--- src/js/main.js
+++ src/js/main.js
@@ -436,7 +436,23 @@ function addCheck(el) {
 saveTaskBtn.addEventListener("click", () => {
     setPriorityColor();
     setPartStr();
-    let taskObject = new taskCard(taskName.value, taskDescription.value, deadline, priorityColor, partStr);
+    setDeadline();
+    setId();
+    let taskObject = new taskCard(
+        taskName.value,
+        taskDescription.value,
+        deadline,
+        priorityColor,
+        partStr,
+        deadlineDate.value,
+        deadlineTime.value,
+        taskId
+    );
     taskObject.createTask();
-    //taskObject.makeObj();
+    taskObject.makeObj();
+    setTaskObjectToStorage();
+    taskName.value = "";
+    taskDescription.value = "";
+    deadlineDate.value = "";
+    deadlineTime.value = "";
 });
