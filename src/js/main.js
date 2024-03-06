d4b93d6b3a29c8cec5cc26ef8e131781ded17bcd
Tatiana Krupnikova <ilm63.rus@gmail.com>
2024-03-06 11:13:30 +0300

    new function

--- src/js/main.js
+++ src/js/main.js
@@ -203,43 +203,11 @@ let taskMemoryObj = {};
 let taskId;
 let arrayFromStorage;
 
-function getTasksStorage() {
-    const JSONarray = window.localStorage.getItem("tasksStorage");
-    arrayFromStorage = JSON.parse(JSONarray);
-}
-
-function setTaskObjectToStorage() {
-    getTasksStorage();
-    arrayFromStorage.push(taskMemoryObj);
-    window.localStorage.setItem("tasksStorage", JSON.stringify(arrayFromStorage));
-}
-
-(function () {
-    let tasksStorage = localStorage.getItem("tasksStorage");
-    tasksStorage = tasksStorage ? JSON.parse(tasksStorage) : [];
-    for (let obj of tasksStorage) {
-        obj.forEach((date) => {
-            let cardObject = new taskCard(
-                date.name,
-                date.description,
-                date.color,
-                date.lifePart,
-                date.deadlineDate,
-                date.deadlineTime,
-                date.id,
-                date.checkbox
-            );
-            cardObject.createTask();
-        });
-    }
-});
-
 function setPriorityColor() {
     let priorityElements = document.forms.taskMaking.elements.prioritybtn;
     for (let i of priorityElements) {
         if (i.checked == true) {
             priorityColor = `${i.value}_lable`;
-            i.checked = "";
         }
     }
 }
@@ -250,51 +218,31 @@ function setPartStr() {
         if (el.checked == true) {
             let currentSpan = el.nextElementSibling;
             partStr = currentSpan.textContent;
-            el.checked = "";
         }
     }
 }
 
 function setDeadline() {
     let startDate = moment();
+    //let startTime
     let taskDeadlinDate = moment(`${deadlineDate.value}T${deadlineTime.value}`);
     deadline = taskDeadlinDate.diff(startDate, "ч.");
 }
 
-function setId() {
-    let idArray = localStorage.getItem("idArray");
-    idArray = idArray ? JSON.parse(idArray) : [];
-    if (idArray.length == 0) {
-        taskId = "taskId1";
-    } else {
-        taskId = `taskId${idArray.length}`;
-    }
-    idArray.push(taskId);
-}
-
-function addCheck(el) {
-    let checkboxId = el.id;
-    getTasksStorage();
-    for (let obj of arrayFromStorage) {
-        obj.forEach((date) => {
-            if (date.id == checkboxId) {
-                date.checkbox = "checked";
-            }
-        });
-    }
-}
-
 class taskCard {
-    constructor(name, description, deadline, color, lifepart, deadlineDate, deadlineTime, id, checkbox) {
+    constructor(name, description, deadline, color, lifepart, deadlineDate) {
         this.name = name;
         this.description = description;
         this.deadline = deadline;
         this.color = color;
         this.lifePart = lifepart;
         this.deadlineDate = deadlineDate;
-        this.deadlineTime = deadlineTime;
-        this.id = id;
-        this.checkbox = checkbox;
+    }
+
+    makeObj() {
+        taskMemoryObj.name = this.name;
+        taskMemoryObj.description = this.description;
+        taskMemoryObj.deadline;
     }
 
     createTask() {
@@ -310,16 +258,12 @@ class taskCard {
         this.element.appendChild(this.priorityLifeEl);
         this.element.appendChild(this.checkEl);
         this.element.appendChild(this.contentBoxEl);
-        this.contentBoxEl.appendChild(this.partLifeEl);
         this.contentBoxEl.appendChild(this.nameEl);
         this.contentBoxEl.appendChild(this.descriptionEl);
         this.element.appendChild(this.deadlineEl);
         this.element.setAttribute("class", "new_task_element");
         this.checkEl.setAttribute("type", "checkbox");
         this.checkEl.setAttribute("class", "task_checkbox");
-        this.checkEl.setAttribute("id", this.id);
-        this.checkEl.setAttribute("onclick", "addCheck(this)");
-        this.partLifeEl.setAttribute("class", "part_life_element");
         this.nameEl.setAttribute("class", "task_name_text");
         this.descriptionEl.setAttribute("class", "description_text");
         this.priorityLifeEl.setAttribute("class", this.color);
@@ -474,16 +418,13 @@ saveTaskBtn.addEventListener("click", () => {
     setPriorityColor();
     setPartStr();
     setDeadline();
-    setId();
     let taskObject = new taskCard(
         taskName.value,
         taskDescription.value,
         deadline,
         priorityColor,
         partStr,
-        deadlineDate.value,
-        deadlineTime.value,
-        taskId
+        deadlineDate.value
     );
     taskObject.createTask();
     taskObject.makeObj();
