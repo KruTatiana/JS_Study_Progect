7701262396118c0d1871c812f0b80cff585b8d41
Nastya <nshpv@icloud.comgit config --global user.email n.shpv@icloud.com'git config --global user.email n.shpv@icloud.com>
2024-03-09 01:52:30 +0300

    added task classes

--- src/js/main.js
+++ src/js/main.js
@@ -180,14 +180,16 @@ let deadline;
 let taskMemoryObj = {};
 let taskId;
 let arrayFromStorage;
+let partValue;
 
 class taskCard {
-    constructor(name, description, deadline, color, lifepart, deadlineDate, deadlineTime, id, checkbox) {
+    constructor(name, description, deadline, color, lifepart, partValue, deadlineDate, deadlineTime, id, checkbox) {
         this.name = name;
         this.description = description;
         this.deadline = deadline;
         this.color = color;
         this.lifePart = lifepart;
+        this.partValue = partValue;
         this.deadlineDate = deadlineDate;
         this.deadlineTime = deadlineTime;
         this.id = id;
@@ -211,11 +213,20 @@ class taskCard {
         this.contentBoxEl.appendChild(this.nameEl);
         this.contentBoxEl.appendChild(this.descriptionEl);
         this.element.appendChild(this.deadlineEl);
-        this.element.setAttribute("class", "new_task_element");
+        this.element.setAttribute("class", `new_task_element ${this.partValue} priority_${this.color} active`);
         this.checkEl.setAttribute("type", "checkbox");
         this.checkEl.setAttribute("class", "task_checkbox");
         this.checkEl.setAttribute("id", this.id);
-        this.checkEl.setAttribute("onclick", "addCheck(this)");
+        this.checkEl.addEventListener("click", () => {
+            console.log(this.checkEl.checked);
+            if (this.checkEl.checked) {
+                this.element.classList.add("done");
+                this.element.classList.remove("active");
+            } else {
+                this.element.classList.add("active");
+                this.element.classList.remove("done");
+            }
+        });
         this.partLifeEl.setAttribute("class", "part_life_element");
         this.nameEl.setAttribute("class", "task_name_text");
         this.descriptionEl.setAttribute("class", "description_text");
@@ -302,6 +313,7 @@ function setPartStr() {
         if (el.checked == true) {
             let currentSpan = el.nextElementSibling;
             partStr = currentSpan.textContent;
+            partValue = el.value;
             el.checked = "";
         }
     }
@@ -318,89 +330,6 @@ function setId() {
     }
     idArray.push(taskId);
 }
-
-function addCheck(el) {
-    let checkboxId = el.id;
-    checkStorage();
-    for (let obj of arrayFromStorage) {
-        obj.forEach((date) => {
-            if (date.id == checkboxId) {
-                date.checkbox = "checked";
-            }
-        });
-    }
-}
-
-function checkStorage() {
-    arrayFromStorage = localStorage.getItem("tasksStorage");
-    arrayFromStorage = arrayFromStorage ? JSON.parse(arrayFromStorage) : [];
-}
-
-function setDeadline() {
-    let startDate = moment();
-    let taskDeadlinDate = moment(`${deadlineDate.value}T${deadlineTime.value}`);
-    deadline = taskDeadlinDate.diff(startDate, "ч.");
-}
-
-function setTaskObjectToStorage() {
-    checkStorage();
-    arrayFromStorage.push(taskMemoryObj);
-    window.localStorage.setItem("tasksStorage", JSON.stringify(arrayFromStorage));
-}
-
-function getTaskList() {
-    setDeadline();
-    checkStorage();
-    for (let obj of arrayFromStorage) {
-        let cardObject = new taskCard(
-            obj.name,
-            obj.description,
-            deadline,
-            obj.color,
-            obj.lifePart,
-            obj.deadlineDate,
-            obj.deadlineTime,
-            obj.id,
-            obj.checkbox
-        );
-        cardObject.createTask();
-    }
-}
-getTaskList();
-
-function setPriorityColor() {
-    let priorityElements = document.forms.taskMaking.elements.prioritybtn;
-    for (let i of priorityElements) {
-        if (i.checked == true) {
-            priorityColor = `${i.value}_lable`;
-            i.checked = "";
-        }
-    }
-}
-
-function setPartStr() {
-    let partElements = document.forms.taskMaking.elements.lifepartbtn;
-    for (let el of partElements) {
-        if (el.checked == true) {
-            let currentSpan = el.nextElementSibling;
-            partStr = currentSpan.textContent;
-            el.checked = "";
-        }
-    }
-}
-
-function setId() {
-    let idArray = localStorage.getItem("idArray");
-    idArray = idArray ? JSON.parse(idArray) : [];
-    if (idArray.length == 0) {
-        taskId = "taskId1";
-    } else {
-        let num = idArray.length + 1;
-        taskId = `taskId${num}`;
-    }
-    idArray.push(taskId);
-}
-
 function addCheck(el) {
     let checkboxId = el.id;
     checkStorage();
@@ -424,6 +353,7 @@ saveTaskBtn.addEventListener("click", () => {
         deadline,
         priorityColor,
         partStr,
+        partValue,
         deadlineDate.value,
         deadlineTime.value,
         taskId
